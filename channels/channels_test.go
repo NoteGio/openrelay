451639344394c8ee32eba6c50771fcb57284142b
Austin Roberts <code@ausiv.com>
2017-08-26 23:02:34 -0500

    Add mock channels and start of channels tests
    This adds a "mock_channels" for in process message passing. It acts more like
    a queue than a topic, in that each message gets relayed only once, and Acking /
    Rejecting is fully implemented.
    
    I've also implemented tests that run for both the mock channels, and if specified
    via environment variables, the redis channesl.

---
+++ channels/channels_test.go
@@ -0,0 +1,201 @@
+package channels_test
+
+import (
+	"github.com/notegio/0xrelay/channels"
+	"gopkg.in/redis.v3"
+	"testing"
+	"os"
+	"fmt"
+)
+
+type testConsumer struct {
+	channel chan string
+	ack chan bool
+	done chan bool
+}
+
+func (consumer *testConsumer)Consume(msg channels.Delivery){
+	consumer.channel <- msg.Payload()
+	if <-consumer.ack {
+		msg.Ack()
+	} else {
+		msg.Reject()
+	}
+	consumer.done <- true
+}
+
+func ChannelSendTest(publisher channels.Publisher, consumerChannel channels.ConsumerChannel, t *testing.T) {
+	fmt.Println("ChannelSendTest")
+	// fmt.Println(publisher)
+	consumer := &testConsumer{make(chan string), make(chan bool), make(chan bool)}
+	// fmt.Println("Created consumer")
+	consumerChannel.AddConsumer(consumer)
+	// fmt.Println("Added consumer")
+	consumerChannel.StartConsuming()
+	// fmt.Println("Started consumer")
+	publisher.Publish("test")
+	// fmt.Println("Published message")
+	result := <-consumer.channel
+	// fmt.Println("Got result")
+	if result != "test" {
+		t.Errorf("Unexpected value")
+	}
+	consumer.ack <- true
+	_ = <-consumer.done
+}
+
+func ReturnUnackedTest(publisher channels.Publisher, consumerChannel channels.ConsumerChannel, t *testing.T) {
+	fmt.Println("ReturnUnackedTest")
+	// fmt.Println(publisher)
+	consumer := &testConsumer{make(chan string), make(chan bool), make(chan bool)}
+	// fmt.Println("Created consumer")
+	consumerChannel.AddConsumer(consumer)
+	// fmt.Println("Added consumer")
+	consumerChannel.StartConsuming()
+	// fmt.Println("Started consumer")
+	publisher.Publish("test")
+	// fmt.Println("Published message")
+	result := <-consumer.channel
+	// fmt.Println("Got result")
+	if result != "test" {
+		t.Errorf("Unexpected value")
+	}
+	// fmt.Println("Checked result")
+	if unackedCount := consumerChannel.ReturnAllUnacked(); unackedCount != 1 {
+		t.Errorf("Expected 1 unacked value, got '%v'", unackedCount)
+	}
+	consumer.ack <- true
+	_ = <-consumer.done
+	// fmt.Println("done here")
+}
+
+func AckTest(publisher channels.Publisher, consumerChannel channels.ConsumerChannel, t *testing.T) {
+	fmt.Println("AckTest")
+	// fmt.Println(publisher)
+	consumer := &testConsumer{make(chan string), make(chan bool), make(chan bool)}
+	// fmt.Println("Created consumer")
+	consumerChannel.AddConsumer(consumer)
+	// fmt.Println("Added consumer")
+	consumerChannel.StartConsuming()
+	// fmt.Println("Started consumer")
+	publisher.Publish("test")
+	// fmt.Println("Published message")
+	result := <-consumer.channel
+	// fmt.Println("Got result")
+	if result != "test" {
+		t.Errorf("Unexpected value")
+	}
+	consumer.ack <- true
+	_ = <-consumer.done
+	if unackedCount := consumerChannel.ReturnAllUnacked(); unackedCount != 0 {
+		t.Errorf("Expected 0 unacked value, got '%v'", unackedCount)
+	}
+}
+
+func RejectTest(publisher channels.Publisher, consumerChannel channels.ConsumerChannel, t *testing.T) {
+	fmt.Println("RejectTest")
+	// fmt.Println(publisher)
+	consumer := &testConsumer{make(chan string), make(chan bool), make(chan bool)}
+	// fmt.Println("Created consumer")
+	consumerChannel.AddConsumer(consumer)
+	// fmt.Println("Added consumer")
+	consumerChannel.StartConsuming()
+	// fmt.Println("Started consumer")
+	publisher.Publish("test")
+	// fmt.Println("Published message")
+	result := <-consumer.channel
+	// fmt.Println("Got result")
+	if result != "test" {
+		t.Errorf("Unexpected value")
+	}
+	consumer.ack <- false
+	_ = <-consumer.done
+	if unackedCount := consumerChannel.ReturnAllUnacked(); unackedCount != 0 {
+		t.Errorf("Expected 0 unacked value, got '%v'", unackedCount)
+	}
+	if purgedCount := consumerChannel.PurgeRejected(); purgedCount != 1 {
+		t.Errorf("Expected 1 purged value, got '%v'", purgedCount)
+	}
+}
+
+func TestMockChannelSend(t *testing.T) {
+	publisher, consumerChannel := channels.MockChannel()
+	ChannelSendTest(publisher, consumerChannel, t)
+}
+func TestMockReturnUnacked(t *testing.T) {
+	publisher, consumerChannel := channels.MockChannel()
+	ReturnUnackedTest(publisher, consumerChannel, t)
+}
+func TestMockAck(t *testing.T) {
+	publisher, consumerChannel := channels.MockChannel()
+	AckTest(publisher, consumerChannel, t)
+}
+func TestMockReject(t *testing.T) {
+	publisher, consumerChannel := channels.MockChannel()
+	RejectTest(publisher, consumerChannel, t)
+}
+
+func redisCleanup(redisClient *redis.Client, consumerChannel channels.ConsumerChannel) {
+	key := "test_queue::unacked"
+	for int(redisClient.LLen(key).Val()) > 0 {
+		redisClient.RPop(key)
+	}
+	consumerChannel.StopConsuming()
+}
+
+func TestRedisQueueChannelSend(t *testing.T) {
+	redisUrl := os.Getenv("REDIS_URL")
+	if redisUrl == "" {
+		t.Errorf("Please set the REDIS_URL environment variable")
+		return
+	}
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: redisUrl,
+	})
+	publisher := channels.NewQueuePublisher("test_queue", redisClient)
+	consumerChannel := channels.NewQueueConsumerChannel("test_queue", redisClient)
+	defer redisCleanup(redisClient, consumerChannel)
+	ChannelSendTest(publisher, consumerChannel, t)
+}
+func TestRedisQueueReturnUnacked(t *testing.T) {
+	redisUrl := os.Getenv("REDIS_URL")
+	if redisUrl == "" {
+		t.Errorf("Please set the REDIS_URL environment variable")
+		return
+	}
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: redisUrl,
+	})
+	publisher := channels.NewQueuePublisher("test_queue", redisClient)
+	consumerChannel := channels.NewQueueConsumerChannel("test_queue", redisClient)
+	defer redisCleanup(redisClient, consumerChannel)
+	ReturnUnackedTest(publisher, consumerChannel, t)
+}
+func TestRedisQueueAck(t *testing.T) {
+	redisUrl := os.Getenv("REDIS_URL")
+	if redisUrl == "" {
+		t.Errorf("Please set the REDIS_URL environment variable")
+		return
+	}
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: redisUrl,
+	})
+	publisher := channels.NewQueuePublisher("test_queue", redisClient)
+	consumerChannel := channels.NewQueueConsumerChannel("test_queue", redisClient)
+	defer redisCleanup(redisClient, consumerChannel)
+	AckTest(publisher, consumerChannel, t)
+}
+func TestRedisQueueReject(t *testing.T) {
+	redisUrl := os.Getenv("REDIS_URL")
+	if redisUrl == "" {
+		t.Errorf("Please set the REDIS_URL environment variable")
+		return
+	}
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: redisUrl,
+	})
+	publisher := channels.NewQueuePublisher("test_queue", redisClient)
+	consumerChannel := channels.NewQueueConsumerChannel("test_queue", redisClient)
+	defer redisCleanup(redisClient, consumerChannel)
+	RejectTest(publisher, consumerChannel, t)
+}
---
+++ channels/mock_channel.go
@@ -0,0 +1,123 @@
+package channels
+
+// import (
+// )
+
+type mockConsumerChannel struct {
+	channel   chan Delivery
+	consumers []Consumer
+	unacked   *deliveries
+	rejected  *deliveries
+}
+
+func (mock *mockConsumerChannel)AddConsumer(consumer Consumer) bool {
+	mock.consumers = append(mock.consumers, consumer)
+	return true
+}
+
+func (mock *mockConsumerChannel)StartConsuming() bool {
+	go func() {
+		for message := range mock.channel {
+			mock.unacked.deliveries = append(mock.unacked.deliveries, message)
+			for _, consumer := range mock.consumers {
+				consumer.Consume(message)
+			}
+		}
+	}()
+	return true
+}
+
+func (mock *mockConsumerChannel)StopConsuming() bool {
+	close(mock.channel)
+	return true
+}
+
+func (mock *mockConsumerChannel)ReturnAllUnacked() int {
+	returned := len(mock.unacked.deliveries)
+	go func () {
+		var message Delivery
+		for {
+			index := len(mock.unacked.deliveries) - 1
+			if index >= 0 {
+				message, mock.unacked.deliveries = mock.unacked.deliveries[index], mock.unacked.deliveries[:index]
+				mock.channel <- message
+			}
+		}
+	}()
+	return returned
+}
+
+func (mock *mockConsumerChannel)PurgeRejected() int {
+	rejected := len(mock.rejected.deliveries)
+	mock.rejected.deliveries = []Delivery{}
+	return rejected
+}
+
+type mockPublisher struct {
+	channel chan Delivery
+	unacked *deliveries
+	rejected *deliveries
+}
+
+func (mock *mockPublisher)Publish(payload string) bool{
+	mock.channel <- &mockDelivery{payload, mock.unacked, mock.rejected}
+	return true
+}
+
+type deliveries struct {
+	deliveries []Delivery
+}
+
+type mockDelivery struct {
+	payload  string
+	unacked  *deliveries
+	rejected *deliveries
+}
+
+func (mock *mockDelivery)Payload() string {
+	return mock.payload
+}
+
+func (mock *mockDelivery)Ack() bool {
+	for i, value := range mock.unacked.deliveries {
+		if value == mock {
+			mock.unacked.deliveries = append(
+				mock.unacked.deliveries[:i],
+				mock.unacked.deliveries[i+1:]...
+			)
+			return true
+		}
+	}
+	return false
+}
+func (mock *mockDelivery)Reject() bool {
+	for i, value := range mock.unacked.deliveries {
+		if value == mock {
+			mock.unacked.deliveries = append(
+				mock.unacked.deliveries[:i],
+				mock.unacked.deliveries[i+1:]...
+			)
+			mock.rejected.deliveries = append(mock.rejected.deliveries, mock)
+			return true
+		}
+	}
+	return false
+}
+
+func MockChannel() (Publisher, ConsumerChannel) {
+	channel := make(chan Delivery, 5)
+	unacked := &deliveries{}
+	rejected := &deliveries{}
+	pub := &mockPublisher{
+		channel,
+		unacked,
+		rejected,
+	}
+	consume := &mockConsumerChannel{
+		channel,
+		[]Consumer{},
+		unacked,
+		rejected,
+	}
+	return pub, consume
+}
