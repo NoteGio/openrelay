6ed47f7e4350e217b92496012e578c545de7f4e2
Austin Roberts <code@ausiv.com>
2017-08-29 22:12:17 -0500

    Add relay construct to channels
    A relay is an object that consumes from a consumer channel, publishes
    to a publisher, and has a filter predicate to decide what gets passed
    and what doesn't. There is an "IncludeAll" filter that doesn't filter
    out any messages, and just passes everything through.

---
+++ channels/relay.go
@@ -0,0 +1,52 @@
+package channels
+
+import (
+	// "log"
+)
+
+// RelayFilter objects provide a predicate function to determine whether a
+// message should be passed to the next stage
+type RelayFilter interface {
+	Filter(Delivery)(bool)
+}
+
+type IncludeAll struct {}
+
+func (filter *IncludeAll)Filter(delivery Delivery)(bool){
+	return true
+}
+
+type Relay struct {
+	consumerChannel ConsumerChannel
+	publisher Publisher
+	filter RelayFilter
+}
+
+func (relay *Relay)Start() bool{
+	return relay.consumerChannel.StartConsuming()
+}
+
+func (relay *Relay)Stop() bool{
+	return relay.consumerChannel.StopConsuming()
+}
+
+type RelayConsumer struct {
+	relay *Relay
+}
+
+func (consumer *RelayConsumer)Consume(delivery Delivery) {
+	if consumer.relay.filter.Filter(delivery) {
+		consumer.relay.publisher.Publish(delivery.Payload())
+	}
+	delivery.Ack()
+}
+
+func NewRelay(channel ConsumerChannel, publisher Publisher, filter RelayFilter) Relay {
+	relay := Relay{
+		channel,
+		publisher,
+		filter,
+	}
+	relay.consumerChannel.AddConsumer(&RelayConsumer{&relay})
+	return relay
+}
---
+++ channels/relay_test.go
@@ -0,0 +1,23 @@
+package channels_test
+
+import (
+	"github.com/notegio/0xrelay/channels"
+	"testing"
+)
+
+func TestRelay(t *testing.T) {
+	sourcePublisher, sourceChannel := channels.MockChannel()
+	destPublisher, destChannel := channels.MockChannel()
+	testConsumer := testConsumer{make(chan string), make(chan bool), make(chan bool)}
+	destChannel.AddConsumer(&testConsumer)
+	destChannel.StartConsuming()
+	relay := channels.NewRelay(sourceChannel, destPublisher, &channels.IncludeAll{})
+	relay.Start()
+	defer relay.Stop()
+	sourcePublisher.Publish("test")
+	message := <-testConsumer.channel
+	if message != "test" {
+		t.Errorf("Message did not get relayed")
+	}
+
+}
