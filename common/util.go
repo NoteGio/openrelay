3a2f7d6a29bdf6950ea9ff528809c5b05a76c7bc
Austin Roberts <code@ausiv.com>
2017-12-09 16:50:40 -0600

    Have HexToBytes strip '0x' from the start, if present

--- common/util.go
+++ common/util.go
@@ -4,6 +4,7 @@ import (
 	"encoding/hex"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/notegio/openrelay/types"
+	"strings"
 )
 
 func BytesToAddress(data [20]byte) common.Address {
@@ -15,7 +16,7 @@ func ToGethAddress(data *types.Address) common.Address {
 }
 
 func HexToBytes(hexString string) ([20]byte, error) {
-	slice, err := hex.DecodeString(hexString)
+	slice, err := hex.DecodeString(strings.TrimPrefix(hexString))
 	result := [20]byte{}
 	if err != nil {
 		return result, err
