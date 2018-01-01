fd2bcd361c9997b25c3538c04dea9cb9f4e00663
Austin Roberts <code@ausiv.com>
2018-01-01 14:20:35 -0600

    Add exchange splitter
    The exchange splitter is intended to be a stage in the pipeline that
    will split messages to different pipelines based on the exchange
    address. This way we can have different pipelines for different exchanges,
    and exchanges are currently our best indicator of network. In the short
    term, this will let us have one pipeline for mainnet and one pipeline for
    ropsten, which will fork right after the ingest stage and right after
    the ordercheckenqueue stage.

---
+++ cmd/exchangesplitter/main.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+	"gopkg.in/redis.v3"
+	"os"
+	"github.com/notegio/openrelay/types"
+	"github.com/notegio/openrelay/channels"
+	"github.com/notegio/openrelay/common"
+	"github.com/notegio/openrelay/splitter"
+	"strings"
+	"log"
+	"os/signal"
+)
+
+func main() {
+	redisURL := os.Args[1]
+	src := os.Args[2]
+	defaultDest := os.Args[3]
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: redisURL,
+	})
+	sourceConsumerChannel, err := channels.ConsumerFromURI(src, redisClient)
+	if err != nil { log.Fatalf(err.Error()) }
+	mapping := make(map[types.Address]channels.Publisher)
+	defaultPublisher, err := channels.PublisherFromURI(defaultDest, redisClient)
+	if err != nil { log.Fatalf(err.Error()) }
+	for _, value := range os.Args[4:] {
+		splitValues := strings.Split(value, "=")
+		addressHex := splitValues[0]
+		publisher, err := channels.PublisherFromURI(splitValues[1], redisClient)
+		if err != nil { log.Fatalf(err.Error()) }
+		addressBytes, err := common.HexToBytes(addressHex)
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		mapping[*common.BytesToOrAddress(addressBytes)] = publisher
+	}
+	exchangeSplitter := splitter.NewExchangeSplitterConsumer(mapping, defaultPublisher)
+	sourceConsumerChannel.AddConsumer(exchangeSplitter)
+	sourceConsumerChannel.StartConsuming()
+	log.Printf("Consuming on '%v'", src)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	for _ = range c {
+		break
+	}
+	sourceConsumerChannel.StopConsuming()
+}
---
+++ splitter/exchange.go
@@ -0,0 +1,30 @@
+package splitter
+
+import (
+	"github.com/notegio/openrelay/channels"
+	"github.com/notegio/openrelay/types"
+)
+
+// ExchangeSplitterConsumer relays orders to different channels based on the
+// exchange specified in the order.
+type ExchangeSplitterConsumer struct {
+	exchanges map[types.Address]channels.Publisher
+	defaultPublisher channels.Publisher
+}
+
+func (consumer *ExchangeSplitterConsumer) Consume(delivery channels.Delivery) {
+	payload := delivery.Payload()
+	orderBytes := [441]byte{}
+	copy(orderBytes[:], []byte(payload))
+	order := types.OrderFromBytes(orderBytes)
+	publisher, ok := consumer.exchanges[*order.ExchangeAddress]
+	if !ok {
+		publisher = consumer.defaultPublisher
+	}
+	publisher.Publish(payload)
+	delivery.Ack()
+}
+
+func NewExchangeSplitterConsumer(exchanges map[types.Address]channels.Publisher, defaultPublisher channels.Publisher) (*ExchangeSplitterConsumer) {
+	return &ExchangeSplitterConsumer{exchanges, defaultPublisher}
+}
