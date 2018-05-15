4b2192faa31da2b78d0e5e2fb7087b3af34224d4
Austin Roberts <code@ausiv.com>
2018-05-15 12:41:45 -0500

    Stop triggering consumers on i/o timeouts

--- channels/util.go
+++ channels/util.go
@@ -3,6 +3,7 @@ package channels
 import (
 	"gopkg.in/redis.v3"
 	"log"
+	"strings"
 )
 
 func redisErrIsNil(result redis.Cmder) bool {
@@ -12,6 +13,9 @@ func redisErrIsNil(result redis.Cmder) bool {
 	case redis.Nil:
 		return true
 	default:
+		if strings.HasSuffix(result.Err().Error(), "i/o timeout") {
+			return true
+		}
 		log.Printf("rmq redis error is not nil %s", result.Err())
 		return false
 	}
