745919219bad42fe76fc4a459f3e6a4d0dad8372
Austin Roberts <code@ausiv.com>
2018-08-27 10:54:14 -0500

    Fix retag script

--- docker-cfg/retag.sh
+++ docker-cfg/retag.sh
@@ -3,6 +3,6 @@
 CONFIG_FILE=$1
 SOURCE_TAG=$2
 NEW_TAG=$3
-WORKING_DIR=$(basename $0)
+WORKING_DIR=$(dirname $0)
 
 python $WORKING_DIR/yaml_tags.py $1 | xargs -I % docker tag %:$SOURCE_TAG %:$NEW_TAG
