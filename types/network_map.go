0420d68f66f23c8cf13583569f0480db628fe7f3
Austin Roberts <code@ausiv.com>
2019-01-14 12:51:47 -0600

    Removing debugging statements that only work if len(items) >= 1

--- types/network_map.go
+++ types/network_map.go
@@ -5,7 +5,6 @@ import (
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/jinzhu/gorm"
 	"errors"
-	"log"
 )
 type NetworkAddressMap map[uint]*Address
 
@@ -19,11 +18,6 @@ func (nmap NetworkAddressMap) Value() (driver.Value, error) {
 	for k, v := range nmap {
 		items = append(items, netAddPair{k, *v})
 	}
-	bytes, err := rlp.EncodeToBytes(items)
-	log.Printf("Saving nmap: %v, %#x, %v", items, bytes[:], err)
-	bytes, err = rlp.EncodeToBytes(&items[0])
-	log.Printf("nmap[0]: %v, %#x, %v", items[0], bytes[:], err)
-
 	return rlp.EncodeToBytes(items)
 }
 
