3c26c26900910db4f28c4d912ba9133b58118830
Austin Roberts <code@ausiv.com>
2019-04-25 11:13:51 -0500

    Allow websockets connections from any origin

--- channels/ws/ws.go
+++ channels/ws/ws.go
@@ -18,6 +18,7 @@ import (
 var upgrader = websocket.Upgrader{
     ReadBufferSize:  1024,
     WriteBufferSize: 1024,
+		CheckOrigin: func(*http.Request) bool { return true },
 }
 
 type websocketDelivery struct {
